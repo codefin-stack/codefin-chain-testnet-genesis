bc2a1983bbc4a2c8b7f817210deedeb0df914aac
thakerng soimadee <thakerng@codefin.io>
2023-07-11 04:37:19 +0700

    Fix script

--- import.sh
+++ import.sh
@@ -7,4 +7,4 @@ cp ./config/app.toml $DATA_DIR/config/app.toml
 cp ./config/client.toml $DATA_DIR/config/client.toml 
 cp ./config/config.toml $DATA_DIR/config/config.toml 
 cp ./config/genesis.json $DATA_DIR/config/genesis.json 
-sed -i "s/moniker = \"orchestrator\"/moniker = "$MONIKER"/g" $DATA_DIR/config/config.toml 
+sed -i "s/moniker = \"orchestrator\"/moniker = \"$MONIKER\"/g" $DATA_DIR/config/config.toml 
